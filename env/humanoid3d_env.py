8f99c207f9d800ba8c2d1d27a6f32d8256ee9101
Mingfei <mingfei.sun.hk@gmail.com>
2019-02-28 20:22:22 +0800

    add testing files

--- env/humanoid3d_env.py
+++ env/humanoid3d_env.py
@@ -3,6 +3,14 @@ import numpy as np
 from gym import utils
 from gym.envs.mujoco import mujoco_env
 
+BODY_JOINTS = ["chest", "neck", "right_shoulder", "right_elbow", 
+               "left_shoulder", "left_elbow", "right_hip", "right_knee", 
+               "right_ankle", "left_hip", "left_knee", "left_ankle"]
+
+DOF_DEF = {"chest": 3, "neck": 3, "right_shoulder": 3, "right_elbow": 1, 
+           "left_shoulder": 3, "left_elbow": 1, "right_hip": 3, "right_knee": 1, 
+           "right_ankle": 3, "left_hip": 3, "left_knee": 1, "left_ankle": 3}
+
 class Humanoid3DEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         self.num_joint = np.nan
@@ -118,8 +126,57 @@ class Humanoid3DEnv(mujoco_env.MujocoEnv, utils.EzPickle):
 
 if __name__ == "__main__":
     env = Humanoid3DEnv()
-    while True:
-        fps = 60
-        update_timestep = 5.0 / fps
-        env.update(update_timestep)
-        env.render()
+    width = 480
+    height = 480
+
+    offset_root_joint = 7
+
+    def calc_vel_from_frames(frame_0, frame_1, dt):
+        curr_idx = 0
+        offset_idx = 7 # root joint offset: 3 (position) + 4 (orientation)
+        vel = []
+        for each_joint in BODY_JOINTS:
+            curr_idx = offset_idx
+            dof = DOF_DEF[each_joint]
+            if dof == 1:
+                offset_idx += dof
+                tmp_vel = (frame_1[curr_idx:offset_idx] - frame_0[curr_idx:offset_idx])*1.0/dt
+                vel += [tmp_vel]
+            elif dof == 3:
+                offset_idx = offset_idx + dof + 1
+                seg0 = frame_0[curr_idx:offset_idx]
+                seg1 = frame_1[curr_idx:offset_idx]
+                q_0 = Quaternion(seg0[0], seg0[1], seg0[2], seg0[3])
+                q_1 = Quaternion(seg1[0], seg1[1], seg1[2], seg1[3])
+                q_diff = q_0.conjugate * q_1
+                axis = q_diff.axis
+                angle = q_diff.angle
+                
+                tmp_vel = (angle * 1.0)/dt * axis
+                vel += [tmp_vel[0], tmp_vel[1], tmp_vel[2], 0]
+
+        return np.array(vel)
+
+    import json
+    from pyquaternion import Quaternion
+
+    motions = None
+    velocities = None
+
+    with open('humanoid3d_backflip.txt') as fin:
+        data = json.load(fin)
+        motions = np.array(data["Frames"])
+        velocities = np.zeros_like(motions)
+        total_time = 0.0
+        for each_frame in motions:
+            duration = each_frame[0]
+            each_frame[0] = total_time
+            total_time += duration
+
+        for idx in range(len(motions) - 1):
+            dt = motions[idx+1][0] - motions[idx][0]
+            frame_0 = motions[idx][1:] # first element is timestamp
+            frame_1 = motions[idx+1][1:]
+
+            velocities[idx, 0] = frame_0[0]
+            velocities[idx, 1 + offset_root_joint:] = calc_vel_from_frames(frame_0, frame_1, dt)
