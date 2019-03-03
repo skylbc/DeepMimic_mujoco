a63cac017843728cba36cd4ffd5a26915eac4d79
Mingfei <mingfei.sun.hk@gmail.com>
2019-03-03 14:50:49 +0800

    add mocap

---
+++ mujoco/mocap.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+import os
+import json
+import copy
+import numpy as np
+from pyquaternion import Quaternion
+from mocap_util import align_position, align_rotation
+from mocap_util import BODY_JOINTS, BODY_JOINTS_IN_DP_ORDER, DOF_DEF
+
+class MocapDM(object):
+    def __init__(self):
+        pass
+
+    def load_mocap(self, filepath):
+        self.read_raw_data(filepath)
+        self.convert_raw_data()
+        return self.mocap_data
+
+    def read_raw_data(self, filepath):
+        motions = None
+        all_states = []
+
+        durations = []
+
+        with open(filepath, 'r') as fin:
+            data = json.load(fin)
+            motions = np.array(data["Frames"])
+            m_shape = np.shape(motions)
+            self.mocap_data = np.full(m_shape, np.nan)
+
+            total_time = 0.0
+            for each_frame in motions:
+                duration = each_frame[0]
+                each_frame[0] = total_time
+                total_time += duration
+                durations.append(duration)
+
+            for each_frame in motions:
+                curr_idx = 1
+                offset_idx = 8
+                state = {}
+                state['root_pos'] = align_position(each_frame[curr_idx:curr_idx+3])
+                state['root_rot'] = align_rotation(each_frame[curr_idx+3:offset_idx])
+                for each_joint in BODY_JOINTS_IN_DP_ORDER:
+                    curr_idx = offset_idx
+                    dof = DOF_DEF[each_joint]
+                    if dof == 1:
+                        offset_idx += 1
+                        state[each_joint] = each_frame[curr_idx:offset_idx]
+                    elif dof == 3:
+                        offset_idx += 4
+                        state[each_joint] = align_rotation(each_frame[curr_idx:offset_idx])
+                all_states.append(state)
+
+        self.all_states = all_states
+        self.durations = durations
+
+    def convert_raw_data(self):
+        for k in range(len(self.all_states)):
+            state = self.all_states[k]
+            dura = self.durations[k]
+
+            # time duration
+            init_idx = 0
+            offset_idx = 1
+            self.mocap_data[k, init_idx:offset_idx] = dura
+
+            # root pos
+            init_idx = offset_idx
+            offset_idx += 3
+            self.mocap_data[k, init_idx:offset_idx] = np.array(state['root_pos'])
+
+            # root rot
+            init_idx = offset_idx
+            offset_idx += 4
+            self.mocap_data[k, init_idx:offset_idx] = np.array(state['root_rot'])
+
+            for each_joint in BODY_JOINTS:
+                init_idx = offset_idx
+                tmp_val = state[each_joint]
+                if DOF_DEF[each_joint] == 1:
+                    assert 1 == len(tmp_val)
+                    offset_idx += 1
+                    self.mocap_data[k, init_idx:offset_idx] = state[each_joint]
+                elif DOF_DEF[each_joint] == 3:
+                    assert 4 == len(tmp_val)
+                    offset_idx += 4
+                    self.mocap_data[k, init_idx:offset_idx] = state[each_joint]
+
+    def play(self, filepath):
+        from mujoco_py import load_model_from_xml, MjSim, MjViewer
+
+        file_path = 'humanoid_deepmimic/envs/asset/humanoid_deepmimic.xml'
+        with open(file_path) as fin:
+            MODEL_XML = fin.read()
+
+        model = load_model_from_xml(MODEL_XML)
+        sim = MjSim(model)
+        viewer = MjViewer(sim)
+
+        self.read_raw_data(filepath)
+        self.convert_raw_data()
+
+        from time import sleep
+
+        phase_offset = np.array([0.0, 0.0, 0.0])
+
+        while True:
+            for k in range(len(self.mocap_data)):
+                tmp_val = self.mocap_data[k, 1:]
+                sim_state = sim.get_state()
+                sim_state.qpos[:] = tmp_val[:]
+                sim_state.qpos[:3] +=  phase_offset[:]
+                sim.set_state(sim_state)
+                sim.forward()
+                viewer.render()
+
+            sim_state = sim.get_state()
+            phase_offset = sim_state.qpos[:3]
+            phase_offset[2] = 0
+
+if __name__ == "__main__":
+    test = MocapDM()
+    test.play("motions/humanoid3d_crawl.txt")
---
+++ mujoco/mocap_util.py
@@ -0,0 +1,54 @@
+import numpy as np
+from pyquaternion import Quaternion
+
+BODY_JOINTS = ["chest", "neck", "right_shoulder", "right_elbow", 
+            "left_shoulder", "left_elbow", "right_hip", "right_knee", 
+            "right_ankle", "left_hip", "left_knee", "left_ankle"]
+
+BODY_JOINTS_IN_DP_ORDER = ["chest", "neck", "right_hip", "right_knee",
+                        "right_ankle", "right_shoulder", "right_elbow", "left_hip", 
+                        "left_knee", "left_ankle", "left_shoulder", "left_elbow"]
+
+DOF_DEF = {"chest": 3, "neck": 3, "right_shoulder": 3, "right_elbow": 1, 
+        "left_shoulder": 3, "left_elbow": 1, "right_hip": 3, "right_knee": 1, 
+        "right_ankle": 3, "left_hip": 3, "left_knee": 1, "left_ankle": 3}
+
+PARAMS_KP_KD = {"chest": [1000, 100], "neck": [100, 10], "right_shoulder": [400, 40], "right_elbow": [300, 30], 
+        "left_shoulder": [400, 40], "left_elbow": [300, 30], "right_hip": [500, 50], "right_knee": [500, 50], 
+        "right_ankle": [400, 40], "left_hip": [500, 50], "left_knee": [500, 50], "left_ankle": [400, 40]}
+
+def align_rotation(rot):
+    q_input = Quaternion(rot[0], rot[1], rot[2], rot[3])
+    q_align_right = Quaternion(matrix=np.array([[1.0, 0.0, 0.0], 
+                                                [0.0, 0.0, 1.0], 
+                                                [0.0, -1.0, 0.0]]))
+    q_align_left = Quaternion(matrix=np.array([[1.0, 0.0, 0.0], 
+                                               [0.0, 0.0, -1.0], 
+                                               [0.0, 1.0, 0.0]]))
+    q_output = q_align_left * q_input * q_align_right
+    return q_output.elements
+
+def align_position(pos):
+    assert len(pos) == 3
+    left_matrix = np.array([[1.0, 0.0, 0.0], 
+                            [0.0, 0.0, -1.0], 
+                            [0.0, 1.0, 0.0]])
+    pos_output = np.matmul(left_matrix, pos)
+    return pos_output
+
+def calc_angular_vel_from_quaternion(orien_0, orien_1, dt):
+    seg0 = align_rotation(orien_0)
+    seg1 = align_rotation(orien_1)
+
+    q_0 = Quaternion(seg0[0], seg0[1], seg0[2], seg0[3])
+    q_1 = Quaternion(seg1[0], seg1[1], seg1[2], seg1[3])
+
+    q_diff =  q_0.conjugate * q_1
+    # q_diff =  q_1 * q_0.conjugate
+    axis = q_diff.axis
+    angle = q_diff.angle
+    
+    tmp_vel = (angle * 1.0)/dt * axis
+    vel_angular = np.array([tmp_vel[0], tmp_vel[1], tmp_vel[2]])
+
+    return vel_angular
